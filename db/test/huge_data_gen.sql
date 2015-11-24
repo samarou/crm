f0917fc694adf6fa61b9470cf1467b58ba9175ae
asamarou <andrei.samarou@itechart-group.com>
2015-11-24 17:28:17 +0300

    script for test data gen

---
+++ db/test/huge_data_gen.sql
@@ -0,0 +1,40 @@
+-- ---------------------------------------------------------------------------------
+
+/*
+INSERT INTO `customer`(`id`, `firstName`, `lastName`, `email`) 
+SELECT @row := @row + 1, concat('firstName', @row), concat('lastName', @row), concat('email', @row) FROM 
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t1,
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t2, 
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t3, 
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t4, 
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t5, 
+(SELECT @row:=10) t0;
+
+INSERT INTO `acl_object_identity`(`id`, `object_type_id`, `object_id`, `parent_id`, `owner_id`, `inheriting`) 
+SELECT @row := @row + 1, 1, @row, null, FLOOR(RAND() * (2)) + 1, true FROM -- FLOOR(RAND() * (<max> - <min> + 1)) + <min>
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t1,
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t2, 
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t3, 
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t4, 
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t5, 
+(SELECT @row:=10) t0;
+
+UPDATE `acl_object_identity`
+SET parent_id = FLOOR(RAND() * (id)) + 11
+where id > 20;
+*/
+
+INSERT INTO `acl_entry`(`id`, `object_identity_id`, `principal_id`, `permission_mask`) 
+SELECT @row := @row + 1, @row - 100100 + 10, FLOOR(RAND() * (2)) + 3, FLOOR(RAND() * (16)) + 1 FROM -- FLOOR(RAND() * (<max> - <min> + 1)) + <min>
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t1,
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t2, 
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t3, 
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t4, 
+(select 0 union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) t5, 
+(SELECT @row:=100100) t0;
+
+-- ---------------------------------------------------------------------------------
+
+analyze table customer;
+analyze table acl_object_identity;
+analyze table acl_entry;
