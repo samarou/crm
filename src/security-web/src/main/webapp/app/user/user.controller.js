4cf142bed92d0fca9a0d6343f0f18beb1fa45bc0
yauheni.putsykovich <yauheni.putsykovich@itechart-group.com>
2016-02-16 13:29:56 +0300

    added roles editing for user on ui.

--- src/security-web/src/main/webapp/app/user/user.controller.js
+++ src/security-web/src/main/webapp/app/user/user.controller.js
@@ -22,8 +22,22 @@ angular.module("app").controller("UserController", ["UserService", "$routeParams
                 vm.user.groups = [
                     {
                         id: 1,
-                        name:"viewers",
+                        name: "viewers",
                         description: "viewers group"
+                    }
+                ];
+                vm.user.roles = [
+                    {
+                        id: 1,
+                        name: "ROOT",
+                        description: "ROOT ROLE",
+                        parent: null
+                    },
+                    {
+                        id: 2,
+                        name: "USER",
+                        description: "USER ROLE",
+                        parent: 1
                     }
                 ];
             });
@@ -31,15 +45,30 @@ angular.module("app").controller("UserController", ["UserService", "$routeParams
 
         vm.unselectGroup = function (group) {
             var index = vm.user.groups.indexOf(group);
-            var b = vm.user.groups;
             vm.user.groups.splice(index, 1);
             vm.groups.push(group);
+            vm.selectedGroup = group;
         };
 
         vm.selectGroup = function (group) {
             var index = vm.groups.indexOf(group);
             vm.groups.splice(index, 1);
             vm.user.groups.push(group);
+            vm.selectedGroup = vm.groups[0];
+        };
+
+        vm.unselectRole = function (role) {
+            var index = vm.user.roles.indexOf(role);
+            vm.user.roles.splice(index, 1);
+            vm.roles.push(role);
+            vm.selectedRole = role;
+        };
+
+        vm.selectRole = function (role) {
+            var index = vm.roles.indexOf(role);
+            vm.roles.splice(index, 1);
+            vm.user.roles.push(role);
+            vm.selectedRole = vm.roles[0];
         };
 
         vm.groups = [
@@ -50,6 +79,22 @@ angular.module("app").controller("UserController", ["UserService", "$routeParams
             }
         ];
         vm.selectedGroup = vm.groups[0];
+
+        vm.roles = [
+            {
+                id: 3,
+                name: "ADMIN",
+                description: "ADMIN ROLE",
+                parent: 2
+            },
+            {
+                id: 4,
+                name: "GUEST",
+                description: "GUEST ROLE",
+                parent: 1
+            }
+        ];
+        vm.selectedRole = vm.roles[0];
 
         function successCallback() {
             $location.path("users");
