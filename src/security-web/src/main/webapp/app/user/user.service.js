1fcc775c79d5faecf3c8781c90638254ec1ff34f
yauheni.putsykovich <yauheni.putsykovich@itechart-group.com>
2016-02-17 17:20:17 +0300

    how fix.

--- src/security-web/src/main/webapp/app/user/user.service.js
+++ src/security-web/src/main/webapp/app/user/user.service.js
@@ -23,31 +23,31 @@
         return service;
 
         function fetchAll(handleSuccess) {
-            return $http.get('rest/users').then(handleSuccess, handleError('Error getting all users'));
+            return $http.get('rest/user').then(handleSuccess, handleError('Error getting all users'));
         }
 
         function getById(id, handleSuccess) {
-            return $http.get('rest/users/' + id).then(handleSuccess, handleError('Error getting user by id'));
+            return $http.get('rest/user/' + id).then(handleSuccess, handleError('Error getting user by id'));
         }
 
         function getByUsername(name, handleSuccess) {
-            return $http.get('rest/users/' + name).then(handleSuccess, handleError('Error getting user by username'));
+            return $http.get('rest/user/' + name).then(handleSuccess, handleError('Error getting user by username'));
         }
 
         function create(user, handleSuccess) {
-            return $http.post('rest/users', user).then(handleSuccess, handleError('Error creating user'));
+            return $http.post('rest/user', user).then(handleSuccess, handleError('Error creating user'));
         }
 
         function update(user, handleSuccess) {
-            return $http.put('rest/users', user).then(handleSuccess, handleError('Error updating user'));
+            return $http.put('rest/user', user).then(handleSuccess, handleError('Error updating user'));
         }
 
         function remove(id) {
-            return $http.delete('rest/users' + id).then(handleSuccess, handleError('Error deleting user'));
+            return $http.delete('rest/user' + id).then(handleSuccess, handleError('Error deleting user'));
         }
 
         function find(filter, successfullyHandler) {
-            return $http.get("rest/users/find", {params: filter}).then(successfullyHandler, handleError("Error during searching of users"))
+            return $http.get("rest/user/find", {params: filter}).then(successfullyHandler, handleError("Error during searching of users"))
         }
 
         function handleSuccess(res) {
