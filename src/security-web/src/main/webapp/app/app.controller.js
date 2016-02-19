8f1623b462aa62fcfe51ef8f5fbbe653133c6331
asamarou <andrei.samarou@itechart-group.com>
2016-02-19 18:42:05 +0300

    restore auth token after page refresh. redirect to login page

--- src/security-web/src/main/webapp/app/app.controller.js
+++ src/security-web/src/main/webapp/app/app.controller.js
@@ -1,9 +1,16 @@
 /**
  * @author yauheni.putsykovich
  */
-angular.module("app").controller("AppController", ["$scope", function ($scope) {
+angular.module("app").controller("AppController", ["$scope", "AuthService", function ($scope, AuthService) {
     "use strict";
+
     console.log("AppController");
+
+    if (!AuthService.isAuthenticated()) {
+        console.log("Restore token");
+        AuthService.restore();
+    }
+
     $scope.partials = {
         menu: "app/partials/menu.partial.html",
         groupModalView: "/app/group/group.modal.view.html"
