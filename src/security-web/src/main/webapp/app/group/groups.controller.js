8269e2b2de2a627f8678fbb67f92f10e571fb97f
yauheni.putsykovich <yauheni.putsykovich@itechart-group.com>
2016-02-19 12:38:49 +0300

    group-ui: added dialog to adding. refactoring.

--- src/security-web/src/main/webapp/app/group/groups.controller.js
+++ src/security-web/src/main/webapp/app/group/groups.controller.js
@@ -1,20 +1,42 @@
 /**
  * @author yauheni.putsykovich
  */
-angular.module("app").controller("GroupsController", ["GroupService", "Collections",
-    function (GroupService, Collections) {
+angular.module("app").controller("GroupsController", ["GroupService",
+    function (GroupService) {
         "use strict";
         var vm = this;
-        vm.group = {};
         GroupService.fetchAll().then(function (response) {
             vm.groupList = response.data;
         });
-        vm.setEditableGroup = function (group) {
+        //vm.group - it's transfer object to pass data to modal and back
+        vm.edit = function (group) {
+            vm.group = {};
             angular.copy(group, vm.group);
+            vm.viewTitle = "Editing Group";
+            vm.actionTitle = "Update";
+            vm.action = update;
         };
-        vm.update = function (group) {
-            var originGroup = vm.groupList.find(function (g) { return g.id === group.id });
+        vm.create = function () {
+            vm.group = {};
+            vm.viewTitle = "Create a New Group";
+            vm.actionTitle = "Add";
+            vm.action = add;
+        };
+
+        function add(group) {
+            vm.groupList.push(group);
+            closeModalView();
+        }
+
+        function update(group) {
+            var originGroup = vm.groupList.find(function (g) {
+                return g.id === group.id
+            });
             angular.copy(group, originGroup);
+            closeModalView();
+        }
+
+        function closeModalView() {
             $("#modal-close-button").trigger("click");//todo: need improve(find other solution)
         }
     }]);
