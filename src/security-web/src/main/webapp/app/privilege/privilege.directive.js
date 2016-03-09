04fbd2729737fe14c168f8d2392df41038f848f1
yauheni.putsykovich <yauheni.putsykovich@itechart-group.com>
2016-03-09 14:55:05 +0300

    hot fixing of privileges list view.

--- src/security-web/src/main/webapp/app/privilege/privilege.directive.js
+++ src/security-web/src/main/webapp/app/privilege/privilege.directive.js
@@ -2,7 +2,7 @@
  * @author yauheni.putsykovich
  */
 
-angular.module("app").directive("privilegeSearch", [function () {
+angular.module("app").directive("privileges", [function () {
     return {
         scope: {
             objectTypes: "="
@@ -10,6 +10,18 @@ angular.module("app").directive("privilegeSearch", [function () {
         require: "objectTypes",
         restrict: "E",
         replace: true,
-        templateUrl: "app/privilege/privilege.partial.view.html"
+        templateUrl: "app/privilege/privilege.partial.view.html",
+        controller: ["$scope", function ($scope) {
+            $scope.selectAll = function (objectType, checked) {
+                objectType.actions.forEach(function (action) {
+                    action.privilege.checked = checked;
+                })
+            };
+            $scope.selectOne = function (objectType) {
+                objectType.isSelectAll = objectType.actions.every(function (action) {
+                    return action.privilege.checked;
+                });
+            };
+        }]
     }
 }]);
