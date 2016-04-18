63c2aa8decd47743ae87f632f34c18ed8b016b47
ToxaBlack <toxablack@gmail.com>
2016-04-18 18:10:03 +0300

    Add service for search in tables

---
+++ src/security-frontend/e2e/search.service.js
@@ -0,0 +1,41 @@
+/*globals require*/
+var SearchService = function () {
+	'use strict';
+
+	var self = this;
+	self.search = function (page, filterFn, searchCriteria) {
+		self.page = page;
+		self.elementName = elementName;
+		self.searchCriteria = searchCriteria;
+		return iterate();
+	};
+
+	function iterate() {
+		var elements = findElements(self.elementName);
+		return elements.count().then(function (count) {
+			if (!count && !self.page.nextPageButton.isEnabled()) {
+				fail('Element didn\'t find');
+			}
+			if (count) {
+				return elements.first();
+			} else {
+				self.page.nextPageButton.click();
+				return iterate();
+			}
+		});
+	}
+
+
+	function findElements(element) {
+		//self.page.searchTab.sendKeys(element);
+		return self.page.pageList().filter(function (row) {
+			return row.element(self.searchCriteria).getText().then(function (text) {
+				return text === element;
+			});
+		})
+	}
+
+};
+
+module.exports = new SearchService();
+
