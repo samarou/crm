addfbe72b2d3fbf84652279775ee0ec516fffd18
ToxaBlack <toxablack@gmail.com>
2016-04-21 10:45:24 +0300

    b543755 change login validation

--- src/security-frontend/src/app/components/login/login.controller.js
+++ src/security-frontend/src/app/components/login/login.controller.js
@@ -5,17 +5,17 @@
 			.module('securityManagement')
 			.controller('LoginController', LoginController);
 
-	function LoginController($state, AuthService, $log) {
+	function LoginController($state, AuthService) {
 		var vm = this;
 		vm.login = function () {
 			AuthService.login(vm.username, vm.password).then(
 					function () {
 						$state.go(AuthService.isAdmin() ? 'users.list' : 'contacts.list');
-					},
-					function (error) {
-						$log.error(error);
+					}).catch(
+					function () {
 						vm.error = 'Invalid login or password';
 					});
-		};
+
+		}
 	}
 })();
