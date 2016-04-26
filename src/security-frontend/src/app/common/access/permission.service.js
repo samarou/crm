03858633f22d25087c2dbe0820e0ba7d1c974139
yauheni.putsykovich <yauheni.putsykovich@itechart-group.com>
2016-04-26 19:55:31 +0300

    #541627 replaced contact-permissions-service on permission-service to reuse it

--- src/security-frontend/src/app/common/access/permission.service.js
+++ src/security-frontend/src/app/common/access/permission.service.js
@@ -2,27 +2,24 @@
  * @author yauheni.putsykovich
  */
 
-/**
- * Created by anton.charnou on 12.04.2016.
- */
 (function () {
   'use strict';
   angular
     .module('securityManagement')
-    .service('ConcatPermissionServiceBuilder', ConcatPermissionServiceBuilder);
+    .service('PermissionServiceBuilder', PermissionServiceBuilder);
 
   /** @ngInject */
-  function ConcatPermissionServiceBuilder($q, ContactService) {
-    return function (getcontact) {
+  function PermissionServiceBuilder($q) {
+    return function (getid, service) {
       return {
         getPermissions: function () {
-          return ContactService.getPermissions(getcontact().id);
+          return service.getPermissions(getid());
         },
         removePermissions: function (permissions) {
           var resultDefer = $q.defer();
           $q.all(
-            permissions.map(function (permission) {
-              return ContactService.removePermissions(getcontact().id, permission.id);
+            ([].concat(permissions)).map(function (permission) {
+              return service.removePermissions(getid(), permission.id);
             })
           ).then(resultDefer.resolve, resultDefer.reject);
           return resultDefer.promise;
