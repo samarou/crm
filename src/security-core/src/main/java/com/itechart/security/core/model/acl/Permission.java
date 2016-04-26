c18806e3b43dcedfb10494fa119e353faae5e6b3
yauheni.putsykovich <yauheni.putsykovich@itechart-group.com>
2016-04-26 20:19:07 +0300

    #541627 added converting mask to permission set

--- src/security-core/src/main/java/com/itechart/security/core/model/acl/Permission.java
+++ src/security-core/src/main/java/com/itechart/security/core/model/acl/Permission.java
@@ -1,5 +1,8 @@
 package com.itechart.security.core.model.acl;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 /**
  * ACL permission
  *
@@ -14,6 +17,16 @@ public enum Permission {
 
     public int getMask() {
         return 1 << ordinal();
+    }
+
+    public static Set<Permission> asSet(int mask) {
+        int sum = 0;
+        Set<Permission> result = EnumSet.noneOf(Permission.class);
+        for (Permission permission : Permission.values()) {
+            sum |= permission.getMask();
+            if(mask >= sum) result.add(permission);
+        }
+        return result;
     }
 
     public boolean isAllow(Permission another) {
