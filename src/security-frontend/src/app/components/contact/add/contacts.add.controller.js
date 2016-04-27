2dffdf51e0fb667b6aaa9e4686d7ef0f98e12965
yauheni.putsykovich <yauheni.putsykovich@itechart-group.com>
2016-04-27 18:34:50 +0300

    #541627 loading default acl list was added

--- src/security-frontend/src/app/components/contact/add/contacts.add.controller.js
+++ src/security-frontend/src/app/components/contact/add/contacts.add.controller.js
@@ -2,17 +2,17 @@
  * Created by anton.charnou on 12.04.2016.
  */
 (function () {
-	'use strict';
+  'use strict';
 
-	angular
-			.module('securityManagement')
-			.controller('ContactsAddController', ContactsAddController);
+  angular
+    .module('securityManagement')
+    .controller('ContactsAddController', ContactsAddController);
 
-	/** @ngInject */
-	function ContactsAddController(ContactService, AclServiceBuilder, PermissionServiceBuilder, $state) {
-		'use strict';
-		var vm = this;
-		vm.contact = {};
+  /** @ngInject */
+  function ContactsAddController(UserService, ContactService, AclServiceBuilder, PermissionServiceBuilder, $state) {
+    'use strict';
+    var vm = this;
+    vm.contact = {};
 
     function getId() {
       return vm.contact.id;
@@ -24,25 +24,28 @@
       actions: AclServiceBuilder(PermissionServiceBuilder(getId, ContactService))
     };
 
-		vm.submitText = 'Add';
-		vm.cancelText = 'Cancel';
-		vm.title = 'Add contact';
+    vm.submitText = 'Add';
+    vm.cancelText = 'Cancel';
+    vm.title = 'Add contact';
 
+    UserService.getPermissionOfCurrentUser().then(function (response) {
+      vm.aclHandler.permissions = response.data;
+    });
 
-		vm.submit = function () {
-			ContactService.create(vm.contact).then(function (response) {
+    vm.submit = function () {
+      ContactService.create(vm.contact).then(function (response) {
         vm.contact.id = response.data;
-				ContactService.updatePermissions(vm.contact.id, vm.aclHandler.permissions).then(
-						function () {
-							$state.go('contacts.list');
-						}
-				)
-			});
+        ContactService.updatePermissions(vm.contact.id, vm.aclHandler.permissions).then(
+          function () {
+            $state.go('contacts.list');
+          }
+        )
+      });
 
-		};
+    };
 
-		vm.cancel = function () {
-			$state.go('contacts.list');
-		};
-	}
+    vm.cancel = function () {
+      $state.go('contacts.list');
+    };
+  }
 })();
