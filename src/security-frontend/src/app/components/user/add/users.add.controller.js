4456f967d07fed21fe7414d99ba59724ed486404
yauheni.putsykovich <yauheni.putsykovich@itechart-group.com>
2016-04-27 18:33:46 +0300

    541627 adding/editing of default-acl list during user creation was added

---
+++ src/security-frontend/src/app/components/user/add/users.add.controller.js
@@ -0,0 +1,64 @@
+(function () {
+	'use strict';
+
+	angular
+			.module('securityManagement')
+			.controller('UsersAddController', UserAddController);
+
+
+	/** @ngInject */
+	function UserAddController(UserService, GroupService, RoleService, AclServiceBuilder, PermissionServiceBuilder, $state) {
+		'use strict';
+		var vm = this;
+
+    vm.submitText = 'Save';
+    vm.cancelText = 'Cancel';
+		vm.title = 'Add user';
+
+    vm.user = {active: 'true'};
+
+    vm.gtoups = [];
+		vm.roles = [];
+    vm.aclHandler = {
+      canEdit: true,
+      permissions:[],
+      actions: AclServiceBuilder(PermissionServiceBuilder(getId, UserService))
+    };
+
+		GroupService.getAll().then(function (response) {
+			vm.groups = response.data;
+		});
+		RoleService.fetchAll().then(function (response) {
+			vm.roles = response.data;
+		});
+
+		vm.submit = function () {
+			checkGroups(vm.user);
+			checkRoles(vm.user);
+      vm.user.acls = vm.aclHandler.permissions;
+			UserService.create(vm.user).then(function () {
+				$state.go('users.list');
+			})
+		};
+
+		vm.cancel = function () {
+			$state.go('users.list');
+		};
+
+    function getId() {
+      return vm.user.id;
+    }
+
+    function checkGroups(user) {
+			user.groups = vm.groups.filter(function (group) {
+				return group.checked;
+			});
+		}
+
+		function checkRoles(user) {
+			user.roles = vm.roles.filter(function (role) {
+				return role.checked;
+			});
+		}
+	}
+})();
---
+++ src/security-frontend/src/app/components/user/edit/users.edit.controller.js
@@ -0,0 +1,90 @@
+/**
+ * Created by anton.charnou on 11.04.2016.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('securityManagement')
+    .controller('UsersEditController', UserEditController);
+
+
+  /** @ngInject */
+  function UserEditController(UserService, GroupService, RoleService, AclServiceBuilder, PermissionServiceBuilder, $state, Collections, $stateParams, $q) {
+    'use strict';
+    var vm = this;
+
+    vm.submitText = 'Save';
+    vm.cancelText = 'Cancel';
+    vm.title = 'Edit user';
+
+    vm.user = {active: 'true'};
+
+    vm.gtoups = [];
+    vm.roles = [];
+    vm.aclHandler = {
+      canEdit: true,
+      permissions: [],
+      actions: AclServiceBuilder(PermissionServiceBuilder(getId, UserService))
+    };
+
+    $q.all(
+      [
+        GroupService.getAll().then(function (response) {
+          vm.groups = response.data;
+        }),
+        RoleService.fetchAll().then(function (response) {
+          vm.roles = response.data;
+        })
+      ]
+    ).then(function () {
+        UserService.getById($stateParams.id).then(function (response) {
+          vm.user = response.data;
+          checkGroupsAndRolesWhichUserHas(vm.user);
+          UserService.getPermissions(getId()).then(function (response) {
+            vm.aclHandler.permissions = response.data;
+          });
+        });
+      });
+
+    vm.submit = function () {
+      checkGroups(vm.user);
+      checkRoles(vm.user);
+      vm.user.acls = vm.aclHandler.permissions;
+      UserService.update(vm.user).then(function () {
+        $state.go('users.list');
+      })
+    };
+
+    vm.cancel = function () {
+      $state.go('users.list');
+    };
+
+    function getId() {
+      return vm.user.id;
+    }
+
+    function checkGroups(user) {
+      user.groups = vm.groups.filter(function (group) {
+        return group.checked;
+      });
+    }
+
+    function checkRoles(user) {
+      user.roles = vm.roles.filter(function (role) {
+        return role.checked;
+      });
+    }
+
+    function checkGroupsAndRolesWhichUserHas(user) {
+      vm.groups.forEach(function (group) {
+        group.checked = !!Collections.find(group, user.groups);
+      });
+      vm.roles.forEach(function (role) {
+        role.checked = !!Collections.find(role, user.roles);
+      });
+    }
+
+
+  }
+})();
