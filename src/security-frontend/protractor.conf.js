969ed9e3f9b7f58f0cb36d7fd6f1607e7f1db7ee
ToxaBlack <toxablack@gmail.com>
2016-04-28 13:43:48 +0300

    add html test report

--- src/security-frontend/protractor.conf.js
+++ src/security-frontend/protractor.conf.js
@@ -1,40 +1,62 @@
 'use strict';
 
 var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
+var path = require('path');
+var Jasmine2HtmlReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new Jasmine2HtmlReporter({
+	dest: 'reports',
+	filename: 'crmReport.html'
+});
 
 exports.config = {
 
-  framework: 'jasmine2',
+	framework: 'jasmine2',
 
-  // The address of a running selenium server.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  baseUrl: 'http://localhost:3000/',
+	// The address of a running selenium server.
+	seleniumAddress: 'http://localhost:4444/wd/hub',
+	baseUrl: 'http://localhost:3000/',
 
-  params: {
-    environment: {
-      // development by default
-      name: 'developer'
-    }
-  },
+	params: {
+		environment: {
+			// development by default
+			name: 'developer'
+		}
+	},
 
-  onPrepare: function () {
-    browser.get('/#/');
-  },
+	beforeLaunch: function () {
+		return new Promise(function (resolve) {
+			reporter.beforeLaunch(resolve);
+		});
+	},
 
-  // seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
+	// Assign the test reporter to each running instance
+	onPrepare: function () {
+		jasmine.getEnv().addReporter(reporter);
+		browser.get('/#/');
+	},
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    browserName: 'chrome'
-  },
+	// Close the report after all tests finish
+	afterLaunch: function (exitCode) {
+		return new Promise(function (resolve) {
+			reporter.afterLaunch(resolve.bind(this, exitCode));
+		});
+	},
 
-  // Spec patterns are relative to the current working directory when
-  // protractor is called.
-   specs: [paths.e2e + '/**/*.spec.js'],
+	// seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
 
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
+	// Capabilities to be passed to the webdriver instance.
+	capabilities: {
+		browserName: 'chrome'
+	},
+
+	// Spec patterns are relative to the current working directory when
+	// protractor is called.
+	specs: [paths.e2e + '/**/*.spec.js'],
+
+	// Options to be passed to Jasmine-node.
+	jasmineNodeOpts: {
+		showColors: true,
+		defaultTimeoutInterval: 30000
+	}
 };
