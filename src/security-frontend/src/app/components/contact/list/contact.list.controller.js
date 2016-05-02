43e4f46b735b194450ec98b7a3a47c9cef93e055
Maksim Kalenik <1995_maxim@mail.ru>
2016-05-02 13:58:16 +0300

    m545216 update code to one style

--- src/security-frontend/src/app/components/contact/list/contact.list.controller.js
+++ src/security-frontend/src/app/components/contact/list/contact.list.controller.js
@@ -1,57 +1,57 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	angular
-			.module('crm.contact')
-			.controller('ContactsListController', ContactsListController);
+    angular
+        .module('crm.contact')
+        .controller('contactListController', contactListController);
 
-	/** @ngInject */
-	function ContactsListController($q, authService, contactService, searchService, dialogService, $state, contactSecurityService) {
-		var vm = this;
+    /** @ngInject */
+    function contactListController($q, authService, contactService, searchService, dialogService, $state, contactSecurityService) {
+        var vm = this;
 
-		vm.isManager = authService.isManager();
-		vm.searchContactBundle = searchService.contactMode();
-		vm.add = add;
-		vm.edit = edit;
-		vm.remove = remove;
+        vm.isManager = authService.isManager();
+        vm.searchContactBundle = searchService.contactMode();
+        vm.add = add;
+        vm.edit = edit;
+        vm.remove = remove;
 
-		init();
+        init();
 
-		function init() {
-			vm.searchContactBundle.find();
-		}
+        function init() {
+            vm.searchContactBundle.find();
+        }
 
-		function add() {
-			$state.go('contacts.add');
-		}
+        function add() {
+            $state.go('contacts.add');
+        }
 
-		function edit(contact) {
-			contactSecurityService.checkReadPermission(contact).then(function () {
-				$state.go('contacts.edit', {id: contact.id});
-			});
-		}
+        function edit(contact) {
+            contactSecurityService.checkReadPermission(contact).then(function () {
+                $state.go('contacts.edit', {id: contact.id});
+            });
+        }
 
-		function remove() {
-			openRemoveDialog().then(function () {
-				var checkedContacts = vm.searchContactBundle.itemsList.filter(function (contact) {
-					return contact.checked;
-				});
-				contactSecurityService.checkDeletePermissionForList(checkedContacts).then(removeContacts)
-			});
-		}
+        function remove() {
+            openRemoveDialog().then(function () {
+                var checkedContacts = vm.searchContactBundle.itemsList.filter(function (contact) {
+                    return contact.checked;
+                });
+                contactSecurityService.checkDeletePermissionForList(checkedContacts).then(removeContacts)
+            });
+        }
 
-		function removeContacts(checkedContacts) {
-			var tasks = [];
-			checkedContacts.forEach(function (contact) {
-				if (contact.checked) {
-					tasks.push(contactService.remove(contact.id));
-				}
-			});
-			$q.all(tasks).then(vm.searchContactBundle.find)
-		}
+        function removeContacts(checkedContacts) {
+            var tasks = [];
+            checkedContacts.forEach(function (contact) {
+                if (contact.checked) {
+                    tasks.push(contactService.remove(contact.id));
+                }
+            });
+            $q.all(tasks).then(vm.searchContactBundle.find)
+        }
 
-		function openRemoveDialog() {
-			return dialogService.confirm('Do you want to delete this contact?').result;
-		}
-	}
+        function openRemoveDialog() {
+            return dialogService.confirm('Do you want to delete this contact?').result;
+        }
+    }
 })();
