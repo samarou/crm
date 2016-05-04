8be14a26fb25bf98f937e0f46116ec9a48ce5515
Maksim Kalenik <1995_maxim@mail.ru>
2016-05-04 17:35:08 +0300

    d543758 added MessengerDto and SocialNetworkDto

---
+++ src/security-web/src/main/java/com/itechart/security/web/model/dto/MessengerDTO.java
@@ -0,0 +1,10 @@
+package com.itechart.security.web.model.dto;
+
+import com.itechart.security.web.model.dto.helper.NamedEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MessengerDto extends NamedEntity{
+}
---
+++ src/security-web/src/main/java/com/itechart/security/web/model/dto/helper/NamedEntity.java
@@ -0,0 +1,11 @@
+package com.itechart.security.web.model.dto.helper;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NamedEntity {
+    private Long id;
+    private String name;
+}
