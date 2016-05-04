db9d3c7fcb79a8efe16c5eaefd90f0fcf1d72583
Maksim Kalenik <1995_maxim@mail.ru>
2016-05-04 18:24:43 +0300

    d545216 uploadHome renamed to uploadDir

--- src/security-frontend/src/app/components/contact/contact.module.js
+++ src/security-frontend/src/app/components/contact/contact.module.js
@@ -1,5 +1,6 @@
 (function () {
     'use strict';
     angular
-        .module('crm.contact', ['crm.common', 'crm.validation', 'crm.formWrapper', 'crm.dialog']);
+        .module('crm.contact', ['crm.common', 'crm.validation', 'crm.formWrapper', 'crm.dialog',
+            'angularFileUpload']);
 })();
