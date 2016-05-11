4869a4ca72c66eab4d1a0f81438b8113475b6bf0
ToxaBlack <toxablack@gmail.com>
2016-05-11 10:27:41 +0300

    fix maven filtering

--- src/security-frontend/src/app/components/footer/footer.directive.js
+++ src/security-frontend/src/app/components/footer/footer.directive.js
@@ -2,14 +2,20 @@
 	'use strict';
 
 	angular
-			.module('crm.footer')
-			.directive('crmFooter', footer);
+		.module('crm.footer')
+		.directive('crmFooter', footer);
 
 	/** @ngInject */
 	function footer() {
 		return {
 			restrict: 'E',
-			templateUrl: 'app/components/footer/footer.template.html'
+			templateUrl: 'app/components/footer/footer.template.html',
+			controller: function (version) {
+				var vm = this;
+
+				vm.version = version;
+			},
+			controllerAs: 'vm'
 		};
 	}
 })();
