938ff5e608b9b2b15c32cc88615dc2939d654944
yauheni.putsykovich <yauheni.putsykovich@itechart-group.com>
2016-05-16 15:04:18 +0300

    #543763 fixed tests

--- src/security-services/src/main/java/com/itechart/security/hibernate/filter/dialect/MySQLFilterFactory.java
+++ src/security-services/src/main/java/com/itechart/security/hibernate/filter/dialect/MySQLFilterFactory.java
@@ -24,17 +24,17 @@ public class MySQLFilterFactory extends AbstractFilterFactory {
         String idColumn = getIdentifierColumn(persistentClass);
         if (filterType == FilterType.ACL_PLAIN) {
             condition.setSqlCondition(
-                "(select ifnull(min(if(aoi.owner_id = :userId, 1, ae.permission_mask >= :permissionMask)), :hasPrivilege)" +
+                "(select min(if(aoi.owner_id = :userId, 1, ae.permission_mask >= :permissionMask)) && hasPrivilege" +
                 " from acl_object_identity aoi left join acl_entry ae on ae.object_identity_id = aoi.id and ae.principal_id in (:principleIds)" +
                 " where aoi.object_type_id = :objectTypeId and aoi.object_id = {alias}." + idColumn + ") > 0");
         } else if (filterType == FilterType.ACL_HIERARCHY) {
             // process ACLs up to second parent (this -> parent1 -> parent2)
             condition.setSqlCondition(
-                "(select ifnull(nullif(min(least(" +
+                "(select nullif(min(least(" +
                 "   ifnull(if(aoi1.owner_id = :userId, 1, ae1.permission_mask >= :permissionMask), 2)," +
                 "   ifnull(if(aoi2.owner_id = :userId, 1, ae2.permission_mask >= :permissionMask), 2)," +
                 "   ifnull(if(aoi3.owner_id = :userId, 1, ae3.permission_mask >= :permissionMask), 2)" +
-                " )), 2), :hasPrivilege) " +
+                " )), 2) && hasPrivilege " +
                 " from acl_object_identity aoi1 " +
                 "   left join acl_object_identity aoi2 on aoi2.id = aoi1.parent_id and aoi1.inheriting = 1" +
                 "   left join acl_object_identity aoi3 on aoi3.id = aoi2.parent_id and aoi2.inheriting = 1" +
