b9f8058894af3d2dabe7db7c7182c4605269d1ac
Maksim Kalenik <1995_maxim@mail.ru>
2016-05-16 15:14:47 +0300

    b553943 added library for showing "loading" state

--- src/security-frontend/src/app/components/contact/contact.module.js
+++ src/security-frontend/src/app/components/contact/contact.module.js
@@ -1,5 +1,5 @@
 (function () {
     'use strict';
     angular
-        .module('crm.contact', ['crm.acl', 'crm.common', 'crm.validation', 'crm.formWrapper', 'crm.dialog', 'angularFileUpload']);
+        .module('crm.contact', ['crm.acl', 'crm.common', 'crm.validation', 'crm.formWrapper', 'crm.dialog', 'angularFileUpload', 'blockUI']);
 })();
