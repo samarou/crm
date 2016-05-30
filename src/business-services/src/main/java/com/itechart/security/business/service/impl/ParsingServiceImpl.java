f4835018c1a8aba040d062bc0bac903777a67ad6
ToxaBlack <toxablack@gmail.com>
2016-05-30 15:06:18 +0300

    #555548 add annotation

--- src/business-services/src/main/java/com/itechart/security/business/service/impl/ParsingServiceImpl.java
+++ src/business-services/src/main/java/com/itechart/security/business/service/impl/ParsingServiceImpl.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.net.URI;
@@ -35,6 +36,7 @@ public class ParsingServiceImpl implements ParsingService {
     private CountryDao countryDao;
 
     @Override
+    @Transactional
     public LinkedInContactDto parse(String profileUrl) {
         log.debug("profile url = {}", profileUrl);
         Document document;
