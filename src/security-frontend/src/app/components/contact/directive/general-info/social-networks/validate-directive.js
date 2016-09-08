7765d77d5d13e0b87bf993828231705c6048a667
aliaksandr.halouchyk <alhalouchyk@gamil.com>
2016-09-08 11:50:24 +0300

    Changes to validate URL in manager profile-Contacts-Social Networks-Add

---
+++ src/security-frontend/src/app/components/contact/directive/general-info/social-networks/validate-directive.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('crm.contact')
+        .directive('validateUrl', isValidateUrl);
+
+    function isValidateUrl() {
+        return {
+            require: 'ngModel',
+            link: function(scope, element, attr, ctrl) {
+                function myValidation(value) {
+                    var data=attr.validateUrl;
+                    if(data==''){
+                        ctrl.$setValidity('isSocialNetworkUrl', false);
+                    }else {
+                        if (data != 'Other') {
+                            var patt = new RegExp(".*" + data.toLowerCase() + ".*");
+                            if (patt.test(value)) {
+                                ctrl.$setValidity('isSocialNetworkUrl', true);
+                            } else {
+                                ctrl.$setValidity('isSocialNetworkUrl', false);
+                            }
+                        }
+                    }
+                    return value;
+                }
+                ctrl.$parsers.push(myValidation);
+            }
+        };
+    }
+})();
